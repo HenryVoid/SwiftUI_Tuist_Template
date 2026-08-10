9267d7758429f316fcba772f5bce0c678716f249
woogi <azicplus@naver.com>
2026-08-11 00:54:35 +0900

    test: GitHubServiceInterface 기본 동작 테스트 추가

--- Projects/Core/GitHubServiceInterface/Project.swift
+++ Projects/Core/GitHubServiceInterface/Project.swift
@@ -8,5 +8,6 @@ let project = Project.makeModule(
     product: .staticLibrary,
     bundleId: bundleID + "GitHubServiceInterface",
     dependencies: [],
-    settings: .core()
+    settings: .core(),
+    hasTest: true
 )
