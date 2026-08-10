df358ea1908c7b887ee86c9932b38716453488da
woogi <azicplus@naver.com>
2026-08-11 00:42:10 +0900

    chore: Tuist 타겟 설정 공통화

--- Plugins/MyAppPlugIn/ProjectDescriptionHelpers/Setting/Setting+.swift
+++ Plugins/MyAppPlugIn/ProjectDescriptionHelpers/Setting/Setting+.swift
@@ -2,54 +2,37 @@ import Foundation
 import ProjectDescription
 
 extension Settings {
-    public static let main: Settings = .settings(
-        base: .init()
-            .swiftCompilationMode(.wholemodule)
-            .automaticCodeSigning(devTeam: devTeam)
-            .currentProjectVersion(buildNumber)
-            .marketingVersion(marketingNumber)
-            .debugInformationFormat(.dwarfWithDsym),
-        configurations: [
-            .debug(name: .debug, xcconfig: .relativeToRoot("XCConfig/Debug.xcconfig")),
-            .release(name: .release, xcconfig: .relativeToRoot("XCConfig/Release.xcconfig"))
-        ]
+    public static let main: Settings = makeSettings(
+        swiftCompilationMode: .wholemodule
     )
-    
+
     public static func base(_ name: String) -> Settings {
-        return .settings(
-            base: .init()
-                .marketingVersion(marketingNumber)
-                .currentProjectVersion(buildNumber)
-                .automaticCodeSigning(devTeam: devTeam)
-                .debugInformationFormat(.dwarfWithDsym),
-            configurations: [
-                .debug(name: .debug, xcconfig: .relativeToRoot("XCConfig/Debug.xcconfig")),
-                .release(name: .release, xcconfig: .relativeToRoot("XCConfig/Release.xcconfig"))
-            ]
-        )
+        return makeSettings()
     }
-    
+
     public static func core() -> Settings {
-        return .settings(
-            base: .init()
-                .marketingVersion(marketingNumber)
-                .currentProjectVersion(buildNumber)
-                .automaticCodeSigning(devTeam: devTeam)
-                .debugInformationFormat(.dwarfWithDsym),
-            configurations: [
-                .debug(name: .debug, xcconfig: .relativeToRoot("XCConfig/Debug.xcconfig")),
-                .release(name: .release, xcconfig: .relativeToRoot("XCConfig/Release.xcconfig"))
-            ]
-        )
+        return makeSettings()
     }
-    
+
     public static func feature(_ name: String) -> Settings {
+        return makeSettings()
+    }
+
+    private static func makeSettings(
+        swiftCompilationMode: SwiftCompilationMode? = nil
+    ) -> Settings {
+        var base = SettingsDictionary()
+            .marketingVersion(marketingNumber)
+            .currentProjectVersion(buildNumber)
+            .automaticCodeSigning(devTeam: devTeam)
+            .debugInformationFormat(.dwarfWithDsym)
+
+        if let swiftCompilationMode {
+            base = base.swiftCompilationMode(swiftCompilationMode)
+        }
+
         return .settings(
-            base: .init()
-                .marketingVersion(marketingNumber)
-                .currentProjectVersion(buildNumber)
-                .automaticCodeSigning(devTeam: devTeam)
-                .debugInformationFormat(.dwarfWithDsym),
+            base: base,
             configurations: [
                 .debug(name: .debug, xcconfig: .relativeToRoot("XCConfig/Debug.xcconfig")),
                 .release(name: .release, xcconfig: .relativeToRoot("XCConfig/Release.xcconfig"))
